fbab613e017ceeef62a9f1919c9af360dac1977b
Sofia <suarezsofia1611@gmail.com>
2023-10-17 13:14:08 -0300

    intento de traer el archivo JSON

---
+++ aaaa.js
@@ -0,0 +1,23 @@
+//const mostrarListado = (data) => {
+//data.forEach(curso => {
+//const tarjCursos = document.createElement(`article`)
+//tarjCursos.setAttribute (`class` , `col-sm-6 mb-3 mb-sm-0`)
+//tarjCursos.innerHTML = `
+ //                         <div id="cajaCurso" class= "card-title">
+   //                       <h1 class= "tituloCurso">  ${curso.nombre} </h1>
+     //                     <h2 class= "textoCurso" > Temas: ${curso.temas} </h2>
+       //                   <h2 class= "textoCurso" > Duración: ${curso.duracion} </h2>
+         //                 <h2 class= "textoCurso" > $${curso.precio} </h2>            
+           //               <button id="inscrbirmeCurso-${curso.nombre}"> Inscribirme</button>
+             //             </div>
+             `
+             //   containerCursos.appendChild(tarjCursos)
+             //   let agregarBtn = document.getElementById(`inscrbirmeCurso-${curso.nombre}`)
+             //   console.log (agregarBtn)
+             //   const inscrbirmeCurso = document.getElementById (`inscrbirmeCurso-${curso.nombre}`)
+             //   inscrbirmeCurso.addEventListener(`click` , () =>{
+             //       agregarAlCarrito(curso.nombre)
+             //     })
+             //   })
+             //  }
+             
--- javascriptDOM.js
+++ javascriptDOM.js
@@ -1,5 +1,4 @@
 let formInscripcion = document.getElementById ("formInscripcion")
-let containerCursos = document.getElementById ("cursos")
 
 function inscripcionEnviada (){
     alert ("¡Gracias por inscribirte! Pronto un representante se pondra en contacto con usted.")
@@ -17,35 +16,35 @@ class Curso{
        this.precio = precio
     }
   }
-  const curso1 = new Curso("Pasteleria","masas, merengues, tartas, tortas", "6 meses", 60000)
-  const curso2 = new Curso("Amateur", "sushi, empanadas, tartas, paellas", "4 meses", 45000)
-  const curso3 = new Curso("Bartender", "caipiriña, fernet, whisky, cervazas", "10 meses", 90000)
-  const curso4 = new Curso("Panes", "minion, frances, masa madre", "3 meses", 40000)
-  const arrayCursos = []
-  arrayCursos.push(curso1,curso2,curso3,curso4)
+  //const curso1 = new Curso("Pasteleria","masas, merengues, tartas, tortas", "6 meses", 60000)
+  //const curso2 = new Curso("Amateur", "sushi, empanadas, tartas, paellas", "4 meses", 45000)
+  //const curso3 = new Curso("Bartender", "caipiriña, fernet, whisky, cervazas", "10 meses", 90000)
+  //const curso4 = new Curso("Panes", "minion, frances, masa madre", "3 meses", 40000)
+  //const arrayCursos = []
+  //arrayCursos.push(curso1,curso2,curso3,curso4)
 
-const mostrarListado = (data) => {
-  data.forEach(curso => {
-   const tarjCursos = document.createElement(`article`)
-   tarjCursos.setAttribute (`class` , `col-sm-6 mb-3 mb-sm-0`)
-   tarjCursos.innerHTML = `
-                          <div id="cajaCurso" class= "card-title">
-                          <h1 class= "tituloCurso">  ${curso.nombre} </h1>
-                          <h2 class= "textoCurso" > Temas: ${curso.temas} </h2>
-                          <h2 class= "textoCurso" > Duración: ${curso.duracion} </h2>
-                          <h2 class= "textoCurso" > $${curso.precio} </h2>            
-                          <button id="inscrbirmeCurso-${curso.nombre}"> Inscribirme</button>
-                          </div>
-                          `
-   containerCursos.appendChild(tarjCursos)
-   const inscrbirmeCurso = document.getElementById (`inscrbirmeCurso-${curso.nombre}`)
-   inscrbirmeCurso.addEventListener(`click` , () =>{
-       agregarAlCarrito(curso.nombre)
-     })
-   })
-  }
-mostrarListado(arrayCursos)
-const carrito = []
+//const mostrarListado = (data) => {
+//data.forEach(curso => {
+//const tarjCursos = document.createElement(`article`)
+//tarjCursos.setAttribute (`class` , `col-sm-6 mb-3 mb-sm-0`)
+//tarjCursos.innerHTML = `
+//                          <div id="cajaCurso" class= "card-title">
+//                          <h1 class= "tituloCurso">  ${curso.nombre} </h1>
+//                          <h2 class= "textoCurso" > Temas: ${curso.temas} </h2>
+//                          <h2 class= "textoCurso" > Duración: ${curso.duracion} </h2>
+//                          <h2 class= "textoCurso" > $${curso.precio} </h2>            
+//                          <button href="#inscripcion" id="inscrbirmeCurso-${curso.nombre}"> Inscribirme</button>
+//                          </div>
+//                          `
+//   containerCursos.appendChild(tarjCursos)
+//   const inscrbirmeCurso = document.getElementById (`inscrbirmeCurso-${curso.nombre}`)
+//   inscrbirmeCurso.addEventListener(`click` , () =>{
+//       agregarAlCarrito(curso.nombre)
+//     })
+//   })
+//  }
+//mostrarListado(arrayCursos)
+//const carrito = []
 function agregarAlCarrito (nombre) {
   let cursoEncontrado = arrayCursos.find (prod => prod.nombre === nombre)
   carrito.push (cursoEncontrado)
@@ -74,3 +73,28 @@ botonModo.addEventListener("click", () => {
         localStorage.setItem("modoOscuro", false)
     }
 })
+function cargaCursos() {
+fetch("cursos.json")
+.then(resp => resp.json())
+.then(data => {
+for (let curso of data) {
+let listadoCurso = new Curso(curso.nombre, curso.temas, curso.duracion, curso.precio);
+array.push(listadoCurso);
+
+let tarjCursos = document.createElement('article');
+tarjCursos.setAttribute('class', 'col-sm-6 mb-3 mb-sm-0');
+tarjCursos.innerHTML = `
+<div id="cajaCurso" class="card-title">
+<h1 class="tituloCurso">${curso.nombre}</h1>
+<h2 class="textoCurso">Temas: ${curso.temas}</h2>
+<h2 class="textoCurso">Duración: ${curso.duracion}</h2>
+<h2 class="textoCurso">Precio: $${curso.precio}</h2>
+<button id="inscribirmeCurso-${curso.nombre}">Inscribirme</button>
+</div>`;
+document.getElementById('cursos').appendChild(tarjCursos);
+}
+})
+.catch(error => console.error('Error cargando cursos:', error));
+}
+
+cargaCursos(array)
